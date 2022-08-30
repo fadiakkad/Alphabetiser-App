0ac8b1d22909aef038f12743d33d0c4165a5a52e
fadiakkad <77630398+fadiakkad@users.noreply.github.com>
2022-08-30 23:02:47 +0100

    Add files via upload

---
+++ Models/ItemViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace ClearLink.Models
+{
+    public class ItemViewModel
+    {
+    
+        public string FirstItem {get;set;}
+        public string SecondItem {get;set;}
+
+       public List<string> ItemsList = new List<string>(); 
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+Ensure that you have .net 5.0 and bootstarp 4/5 installed on your device.
+
+1- Open the app 
+2- Run it
